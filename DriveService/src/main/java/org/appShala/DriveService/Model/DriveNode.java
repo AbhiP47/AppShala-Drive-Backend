ece0886b1367b00ad68f5f659d6d72cd069907b1
Sanchita Dang <sanchitadang2004@gmail.com>
2025-10-30 13:48:56 +0530

    feat: Implement core persistence layer (Entities, Repositories, Enums)
    This commit introduces the complete Model and Repository layers for the DriveService API,
    based on the defined database schema.

--- DriveService/src/main/java/org/appShala/DriveService/Model/DriveNode.java
+++ DriveService/src/main/java/org/appShala/DriveService/Model/DriveNode.java
@@ -1,9 +1,13 @@
 package org.appShala.DriveService.Model;
 
+import org.appShala.DriveService.Model.DriveNode;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Data
 @AllArgsConstructor
@@ -14,6 +18,48 @@ public class DriveNode {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private UUID id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "NodeType")
+    private UUID nodeType;
+
+    @ManyToOne(fetch =FetchType.LAZY)
+    @JoinColumn(name = "parentId" , referencedColumnName = "id")
+    private DriveNode parentNode;
+
+    @Column(name="StorageId")
+    private UUID storageId;
+
+    @Column(name= "sizeBytes")
+    private Long sizeBytes;
+
+    @Column(name="created_at" , nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "lastModifiedAt" , nullable = false)
+    private LocalDateTime lastModifiedAt;
+
+    @Column(name = "Shared_With")
+    private Boolean sharedWith;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "is_deleted")
+    private Boolean isDeleted;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+
+    @Column(name = "ownerId")
+    private UUID ownerId;
+
+    @Column(name= "modifiedBy")
+    private UUID modifiedByID;
+
+
 
 }
